a118d2198c87fe86d2f863a4f53a0d2d06bd7619
Unknown <guillaume.biausque@gmail.com>
2019-02-13 08:09:47 +0100

    Création d'un header et d'un script contenant tout les liens nécessaire à l'app

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -11,8 +11,10 @@
 
         <!-- Fonts/Police -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Fredericka+the+Great|Nothing+You+Could+Do" rel="stylesheet">
         <!-- Bootstrapp -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css" integrity="sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
         <!-- Bootstrapp toggle -->
         <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
         <!-- Modification Css -->
@@ -21,5 +23,6 @@
         <link href="{{ asset('css/mycss.css') }}" rel="stylesheet">
     </head>
     <body> 
+        
    
      
